3c49e91d17a9f44c36510f6c5edaf08108089d84
Steven Turner <tstupidt@users.noreply.github.com>
2019-01-24 15:02:34 -0800

    Generators, blackjack, and final projects

---
+++ Notes/Section 10 (errors and testing)/cap.py
@@ -0,0 +1,6 @@
+def cap_text(text):
+	'''
+	Input is a string
+	Output is a string with capital first letter
+	'''
+	return text.title()
---
+++ Notes/Section 10 (errors and testing)/simple1.py
@@ -0,0 +1,14 @@
+'''
+A very simple script
+'''
+
+def myfunc():
+    '''
+    A simple Function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
